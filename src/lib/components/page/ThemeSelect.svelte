9ef5beb0934db59309b1e4e6d6c38ee4e0cb175d
Travis Fantina <tfantina@gmail.com>
2023-12-18 20:35:46 -0800

    Adds theming

---
+++ src/lib/components/page/ThemeSelect.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let setTheme, currentTheme;
+</script>
+
+<div class="theme-box">
+    <button class="themable forest" on:click={() => {setTheme("forest")}}></button>
+    <button class="themable creamsicle" on:click={() => {setTheme("creamsicle")}}></button>
+    <button class="themable nightly" on:click={() => {setTheme("nightly")}}></button>
+    <span class="divider"></span>
+    <button class="themable {$currentTheme}"></button>
+</div>
+
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,29 @@
 <script>
+    import { onMount } from 'svelte'
+    import { writable } from 'svelte/store';
     import Header from '$lib/components/page/Header.svelte';
     import Nav from '$lib/components/page/Nav.svelte';
     import MobileFooter from '$lib/components/page/MobileFooter.svelte'
+    import ThemeSelect from '$lib/components/page/ThemeSelect.svelte'
     import '$lib/scss/global.scss'
     import { page } from '$app/stores';
+
+    let theme;
+    let currentTheme = writable(theme);
+
+    const setTheme = (theme) => {
+        window.localStorage.setItem('theme', theme);
+        currentTheme.set(theme) 
+        document.querySelector('html').dataset.theme = theme;
+    }
+
+    onMount(() => {
+        theme = window.localStorage.getItem('theme');
+        theme ? setTheme(theme) : setTheme('forest')
+    })
     
 </script>
+<ThemeSelect {setTheme} {currentTheme} />
 <div class="container">
     <Header>
         <span slot="title">{$page.data.title}</span>
