d3cddda0817e6c0b5ab1819f462dc9d7665af7b9
Travis Fantina <tfantina@gmail.com>
2023-12-18 20:41:33 -0800

    Setup github pages

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import adapter from '@sveltejs/adapter-static'
 
 export default defineConfig({
 	plugins: [sveltekit()],
@@ -12,5 +13,19 @@ export default defineConfig({
 				`
 			}
 		}
+	},
+
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	}
 });
+
