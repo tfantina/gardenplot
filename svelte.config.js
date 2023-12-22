4e17aaee24d4da3f57c53cb5851b736bbdcb93fb
Travis Fantina <tfantina@gmail.com>
2023-12-22 09:11:55 -0800

    Updates config

--- svelte.config.js
+++ svelte.config.js
@@ -5,10 +5,17 @@ import adapter from '@sveltejs/adapter-static'
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(fallback: '404.html'),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
+		adapter: adapter({
+			fallback: '404.html',
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precrompress: false,
+			struct: true,
+			paths: {
+				base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			}
+		})
 	},
 	extensions: ['.svelte', '.md'],
 	preprocess: [vitePreprocess(), mdsvex({ extensions: ['.md'] })],
