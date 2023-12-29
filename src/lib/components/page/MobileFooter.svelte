bff1a2427c3e6f252d16a2ca7bd5ac2e30320f28
Travis Fantina <tfantina@gmail.com>
2023-12-28 21:30:43 -0800

    Fixes to mobile footer

--- src/lib/components/page/MobileFooter.svelte
+++ src/lib/components/page/MobileFooter.svelte
@@ -1,8 +1,11 @@
 <script>
+    import { base } from '$app/paths';
     import NavButton from '$lib/components/page/NavButton.svelte';
 </script>
 <footer class="col align-items-center">
-    <NavButton text="Programming" target={"/programming"} />
-    <NavButton text="Visuals" target={"/visuals"} />
-    <NavButton text="Now" target={"/now"} />
+    <NavButton text="Home" target={`${base}/`} />
+    <NavButton text="Programming" target={`${base}/programming`} />
+    <NavButton text="Visuals" target={`${base}/visuals`} />
+    <NavButton text="Projects" target={`${base}/projects`} />
+    <NavButton text="About" target={`${base}/about`} />
 </footer>
